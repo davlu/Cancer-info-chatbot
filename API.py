37d38af7074d1bc0c128c9dcfb30ba628c2b2816
David L <davidlu1111@gmail.com>
2017-10-08 17:58:22 -0500

    Update API.py

--- API.py
+++ API.py
@@ -1,8 +1,7 @@
-import os.path
-import sys
 import json
 import apiai
 from google import search
+import requests, bs4
 
 CLIENT_ACCESS_TOKEN = '871dda22df9b4278b2c1f7de27d5b9ab'
 
@@ -11,7 +10,28 @@ def websitelink(symptom):
     for result in search(symptom + ' symptom mayo clinic', stop = 20):
         list.append(result)
     url = list[0]
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text)
     print(url)
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html5lib')
+    find_Match = soup.find('p', text='Signs and symptoms of ' + symptom + ' may include:')
+    soup2 = bs4.BeautifulSoup(res.text, 'html5lib')
+    loc = soup2.prettify().index('Signs and symptoms of ' + symptom + ' may include:')
+    soup2 = bs4.BeautifulSoup(soup2.prettify()[loc:], 'html5lib')
+    find_Match2 = soup2.find('ul')
+    list = []
+    for item in find_Match2:
+        list.append(str(item))
+    symptomsList = []
+    for item in list:
+        item = item.replace('\n', '').replace('<li>', '').replace('</li>', '').strip()
+        if item != '':
+            symptomsList.append(item)
+    print(find_Match.text)
+    for x in symptomsList:
+        print('-' + x)
 
 def main():
     user_input = ''
